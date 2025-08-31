44f003dde9e0aede6ecd28d1c35402f9e97e8155
EBF <fingerhut22@gmail.com>
2025-08-31 10:49:03 -0400

    test: split TOML and JSON tests into separate files
    Separated TOML and JSON test cases for `ConfigService` into distinct files for better modularity and clarity. Updated fixtures and test implementations to align with the new structure. Retained test coverage for both formats.

--- EbfUtil/tests/ebfutil/cfgutil/cfg_svc_tests/handlers/json/test_load.py
+++ EbfUtil/tests/ebfutil/cfgutil/cfg_svc_tests/handlers/json/test_load.py
@@ -7,28 +7,28 @@ from ebfutil.cfgutil import ConfigService
 from tests.ebfutil.cfgutil.fixtures.cfg_svc_fixture import ConfigServiceFixture
 
 
-class TomlConfigServiceFixture(ConfigServiceFixture):
+class JsonConfigServiceFixture(ConfigServiceFixture):
     @pytest.fixture(scope="class")
     def default_ext(self) -> str:
-        return ".toml"
+        return ".json"
 
     @pytest.fixture(scope="class")
-    def toml_cfg_file(self, make_filename) -> str:
+    def json_cfg_file(self, make_filename) -> str:
         return make_filename()
 
     @pytest.fixture
-    def project_file(self, toml_cfg_file: str, project_root: Path, project_search_path: str, data: dict) -> Path:
-        tgt = project_root / project_search_path / toml_cfg_file
+    def project_file(self, json_cfg_file: str, project_root: Path, project_search_path: str, data: dict) -> Path:
+        tgt = project_root / project_search_path / json_cfg_file
         tgt.parent.mkdir(parents=True, exist_ok=True)
         tgt.write_text(json.dumps(data), encoding="utf-8")
         return tgt
 
     @staticmethod
-    def test_fixture_overrides(toml_cfg_file):
-        assert toml_cfg_file == "config.toml"
+    def test_fixture_overrides(json_cfg_file):
+        assert json_cfg_file == "config.json"
 
 
-class TestJsonLoad(TomlConfigServiceFixture):
+class TestJsonLoad(JsonConfigServiceFixture):
 
     def test_can_load_project_config(self, sut: ConfigService, app_name, json_cfg_file,
                                      project_file_util, project_file: Path, data: dict
