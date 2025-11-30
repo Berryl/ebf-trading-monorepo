f2b5b8f3583b3e54ad8e1e6281e5b5ee0ac5366b
EBF <fingerhut22@gmail.com>
2025-11-30 17:19:07 -0500

    refactor: add `ConfigFormatHandler` and re-export all handlers in `cfgutil`
    Introduced `ConfigFormatHandler` in `handlers` and updated the `__all__` exports in `cfgutil` to include it alongside other handlers. Simplified imports by re-exporting all handler types and key modules directly from `cfgutil`.

--- EbfUtil/src/ebf_core/cfgutil/__init__.py
+++ EbfUtil/src/ebf_core/cfgutil/__init__.py
@@ -1,5 +1,17 @@
-# from .cfg_service import ConfigService
-#
-#
-#
-# __all__ = ["load_config", "store_config", "update_config"]
+from .cfg_service import ConfigService
+from .cfg_merger import ConfigMerger
+
+# Convenience re-exports for handler types
+from .handlers.cfg_format_handler import ConfigFormatHandler
+from .handlers.json_handler import JsonHandler
+from .handlers.yaml_handler import YamlHandler
+from .handlers.toml_handler import TomlHandler
+
+__all__ = [
+    "ConfigService",
+    "ConfigMerger",
+    "ConfigFormatHandler",
+    "JsonHandler",
+    "YamlHandler",
+    "TomlHandler",
+]
