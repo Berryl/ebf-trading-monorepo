c42eeec19eb7fe0bafa52a32a718738b571d59e0
Berryl <ef7470ef@gmail.com>
2026-01-19 20:22:21 -0500

    Add unit test for `Strike` value object's `from_amount` method

---
+++ EbfTrading/tests/value_objects/test_strike.py
@@ -0,0 +1,10 @@
+import pytest
+from ebf_domain.money.money import Money
+
+from ebf_trading.domain.value_objects.options.strike import Strike
+
+
+class TestStrike:
+    def test_from_amount(self):
+        strike = Strike.from_amount(100.50)
+        assert strike.price == Money.mint(100.50)
