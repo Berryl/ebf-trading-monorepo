7d6ad947a185a859d8df826d971463ea17d2bcb5
Berryl <ef7470ef@gmail.com>
2026-01-19 20:34:49 -0500

    Add unit test for `Strike` value object's initialization

--- EbfTrading/tests/value_objects/test_strike.py
+++ EbfTrading/tests/value_objects/test_strike.py
@@ -1,4 +1,3 @@
-import pytest
 from ebf_domain.money.currency import USD
 from ebf_domain.money.money import Money
 
@@ -6,6 +5,10 @@ from ebf_trading.domain.value_objects.options.strike import Strike
 
 
 class TestStrike:
+    def test_init(self):
+        m = Money(50, USD)
+        assert Strike(m).price == m
+
     def test_from_amount(self):
         strike = Strike.from_amount(100.50)
         assert strike.price == Money.mint(100.50, USD)
