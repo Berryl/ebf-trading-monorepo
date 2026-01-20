3da04678cb31af314b6545df0949cbd37aaa51d5
Berryl <ef7470ef@gmail.com>
2026-01-20 12:57:38 -0500

    Add `opex_time` and `opex_tz` fields to `ExpirationDate` for enhanced time zone handling

--- EbfTrading/src/ebf_trading/domain/value_objects/options/expiration_date.py
+++ EbfTrading/src/ebf_trading/domain/value_objects/options/expiration_date.py
@@ -1,11 +1,17 @@
-from dataclasses import dataclass
-from datetime import datetime
+from dataclasses import dataclass, field
+from datetime import datetime, time
+from zoneinfo import ZoneInfo
+
 from ebf_core.guards import guards as g
 
 
 @dataclass(frozen=True)
 class ExpirationDate:
     when: datetime
+    make__opex_time: bool = True
+    opex_time: time = field(default_factory=lambda: time(17, 30))
+    opex_tz: ZoneInfo = field(default_factory=lambda: ZoneInfo("America/New_York"))
+
 
     def to_occ_fmt(self):
         g.ensure_not_none(self.when, "when")
